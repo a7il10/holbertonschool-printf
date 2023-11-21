d7fac798e5b00fe4dc12b652d579d0185d1c6011
a7il10 <atilio.sulovari199@gmail.com>
2023-11-21 02:52:54 -0800

    main.h

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+typedef struct print_data
+{
+	char *type;
+	int (*print)(va_list list);
+}print_data;
+
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list char_list);
+int print_string(va_list string_list);
+
+#endif
