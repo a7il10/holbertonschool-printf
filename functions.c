fbcbabb5c6a011b9b2d306ebdf961ad1acd2d56f
MDrenova <mateodrenova@gmail.com>
2023-11-24 04:57:01 -0800

    Update betty style

--- functions.c
+++ functions.c
@@ -11,11 +11,11 @@
 
 int print_c(va_list c_list)
 {
-        char s;
+	char s;
 
-        s = va_arg(c_list, int);
-        _putchar(s);
-        return (1);
+	s = va_arg(c_list, int);
+	_putchar(s);
+	return (1);
 }
 
 /**
@@ -26,16 +26,16 @@ int print_c(va_list c_list)
 
 int print_s(va_list s_list)
 {
-        int count;
-        char *str = va_arg(s_list, char *);
+	int count;
+	char *str = va_arg(s_list, char *);
 
-        if (str == NULL)
-                str = "(null)";
-        for (count = 0; str[count]; count++)
-        {
-                _putchar(str[count]);
-        }
-        return (count);
+	if (str == NULL)
+		str = "(null)";
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+	return (count);
 }
 
 /**
@@ -46,31 +46,31 @@ int print_s(va_list s_list)
 
 int print_i(va_list i_list)
 {
-        long int number = (long int)va_arg(i_list, int);
-        long int l = 1;
-long int t = number;
-        int r = 1;
+	long int number = (long int)va_arg(i_list, int);
+	long int l = 1;
+	long int t = number;
+	int r = 1;
 
-        if (number < 0)
-        {
-                _putchar('-');
-                r++;
-                t = t * -1;
-                number = number * -1;
-        }
-        while (t > 9)
-        {
-                t = t / 10;
-                l = l * 10;
-                r++;
-        }
-        while (l > 0)
-        {
-                _putchar('0' + (number / l) % 10);
-                l = l / 10;
-        }
+	if (number < 0)
+	{
+		_putchar('-');
+		r++;
+		t = t * -1;
+		number = number * -1;
+	}
+	while (t > 9)
+	{
+		t = t / 10;
+		l = l * 10;
+		r++;
+	}
+	while (l > 0)
+	{
+		_putchar('0' + (number / l) % 10);
+		l = l / 10;
+	}
 
-        return (r);
+	return (r);
 }
 
 /**
@@ -81,32 +81,30 @@ long int t = number;
 
 int print_d(va_list d_list)
 {
-        long int number = (long int)va_arg(d_list, int);
-        long int l = 1;
-        long int t = number;
-        int r = 1;
+	long int number = (long int)va_arg(d_list, int);
+	long int l = 1;
+	long int t = number;
+	int r = 1;
 
-        if (number < 0)
-        {
-                _putchar('-');
-                r++;
+	if (number < 0)
+	{
+		_putchar('-');
+		r++;
 		t = t * -1;
-                number = number * -1;
-        }
-        while (t > 9)
-        {
-                t = t / 10;
-                l = l * 10;
-                r++;
-        }
-        while (l > 0)
-        {
-                _putchar('0' + (number / l) % 10);
-                l = l / 10;
-        }
+		number = number * -1;
+	}
+	while (t > 9)
+	{
+		t = t / 10;
+		l = l * 10;
+		r++;
+	}
+	while (l > 0)
+	{
+		_putchar('0' + (number / l) % 10);
+		l = l / 10;
+	}
 
-        return (r);
+	return (r);
 }
 
-                                                     
-                                              
