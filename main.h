ac9d3f9ef743de43dee5de51b6e90c4a73158f59
MDrenova <104514975+MDrenova@users.noreply.github.com>
2023-11-24 13:26:41 +0100

    Update main.h

--- main.h
+++ main.h
@@ -2,23 +2,22 @@
 #define MAIN_H
 
 #include <stdarg.h>
-/**
- * struct print - Structure containing identifiers
- * @t: type to print
- * @f: function to print
+
+/** 
+ * Struct for format specifiers and corresponding print functions 
  */
-typedef struct print
+typedef struct format
 {
-	char *t;
-	int (*f)(va_list);
-} print_t;
+    char *spec;
+    int (*f)(va_list);
+} format_t;
 
-int _putchar(char c);
-int (*check_formats(const char *format))(va_list);
+/** 
+ * Function prototypes 
+ */
 int _printf(const char *format, ...);
-int print_c(va_list ch_list);
-int print_s(va_list s_list);
-int print_i(va_list i_list);
-int print_d(va_list d_list);
+int print_char(va_list list);
+int print_string(va_list list);
+int print_int(va_list list);
 
-#endif
+#endif 
